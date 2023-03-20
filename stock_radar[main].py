aed034402f4b6990c0c8c11dde81e16d637c661e
Sang rongyong <s6401012620242@email.kmutnb.ac.th>
2023-03-20 19:03:40 +0700

    test case

--- stock_radar[main].py
+++ stock_radar[main].py
@@ -722,6 +722,246 @@ class TestMLStock(unittest.TestCase):
             # assert that the result is as expected
             assert_frame_equal(result, for_test)
 
+    @patch('yfinance.download')
+    def test_download_stock(self,mock_download):
+        Ticker = 'AAPL'
+        # Create mock objects for the yf.download method
+        yf_mock = MagicMock()
+
+        mock_download.side_effect = [
+            yf_mock,yf_mock,yf_mock
+        ]
+
+        # Call the function with a mock Ticker
+        self.stock.download_stock(Ticker)
+
+        # Assert that the mock functions were called with the expected arguments
+        mock_download.assert_called_with(tickers=Ticker, period='max', interval='1mo')
+
+    @patch('pandas.read_sql')
+    def test_download_year_nasdaq(self,mock_read_sql):
+        webdriver.Chrome = MagicMock()
+        mock_driver = MagicMock()
+        webdriver.Chrome.return_value = mock_driver
+        # Set up the mock DataFrame
+        df = pd.DataFrame({'Index':['NASDAQ']})
+        mock_read_sql.return_value = df
+        reesult = self.stock.download_year('AAPL',False)
+        mock_driver.get.assert_called_once_with('https://www.tradingview.com/symbols/NASDAQ-AAPL/financials-income-statement/')
+    
+    @patch('pandas.read_sql')
+    def test_download_year_set(self,mock_read_sql):
+        webdriver.Chrome = MagicMock()
+        mock_driver = MagicMock()
+        webdriver.Chrome.return_value = mock_driver
+        # Set up the mock DataFrame
+        df = pd.DataFrame({'Index':['SET100']})
+        mock_read_sql.return_value = df
+        reesult = self.stock.download_year('AAV',False)
+        mock_driver.get.assert_called_once_with('https://www.tradingview.com/symbols/SET-AAV/financials-income-statement/')
+
+    @patch('pandas.read_sql')
+    def test_download_quarter(self,mock_read_sql):
+        webdriver.Chrome = MagicMock()
+        mock_driver = MagicMock()
+        webdriver.Chrome.return_value = mock_driver
+        mock_header = MagicMock()
+        mock_raw1 = MagicMock()
+        mock_raw2 = MagicMock()
+        mock_raw3 = MagicMock()
+        mock_raw4 = MagicMock()
+        mock_raw5 = MagicMock()
+        mock_raw6 = MagicMock()
+        mock_raw7 = MagicMock()
+        mock_raw8 = MagicMock()
+        mock_raw9 = MagicMock()
+        mock_raw10 = MagicMock()
+        mock_raw11 = MagicMock()
+        mock_raw12 = MagicMock()
+        mock_raw13 = MagicMock()
+        mock_raw14 = MagicMock()
+        mock_raw15 = MagicMock()
+        mock_raw16 = MagicMock()
+        mock_raw17 = MagicMock()
+        mock_raw18 = MagicMock()
+        mock_raw19 = MagicMock()
+        mock_raw20 = MagicMock()
+        mock_raw21 = MagicMock()
+        mock_raw22 = MagicMock()
+        mock_raw23 = MagicMock()
+        mock_raw24 = MagicMock()
+
+        mock_header.text = "Currency: USD\n2022\nTTM"
+        mock_raw1.text = 'Total revenue\nYoY growth\n\u202a117.15B\u202c\n−5.48%\n\u202a387.54B\u202c'
+        mock_raw2.text = 'Cost of goods sold\n\u202a−66.82B\u202c\n\u202a−220.67B\u202c'
+        mock_raw3.text = 'Gross profit\nYoY growth\n\u202a50.33B\u202c\n−7.21%\n\u202a166.87B\u202c'
+        mock_raw4.text = 'Operating expenses (excl. COGS)\n\u202a−14.32B\u202c\n\u202a−52.91B\u202c'
+        mock_raw5.text = 'Operating income\nYoY growth\n\u202a36.02B\u202c\n−13.19%\n\u202a113.97B\u202c'
+        mock_raw6.text = 'Non-operating income, total\n\u202a−393.00M\u202c\n\u202a−480.00M\u202c'
+        mock_raw7.text = 'Pretax income\nYoY growth\n\u202a35.62B\u202c\n−13.62%\n\u202a113.48B\u202c'
+        mock_raw8.text = 'Equity in earnings\n\u202a0.00\u202c\n—'
+        mock_raw9.text = 'Taxes\n\u202a−5.63B\u202c\n\u202a−18.31B\u202c'
+        mock_raw10.text = 'Non-controlling/minority interest\n\u202a0.00\u202c\n—'
+        mock_raw11.text = 'After tax other income/expense\n\u202a0.00\u202c\n—'
+        mock_raw12.text = 'Net income before discontinued operations\n\u202a30.00B\u202c\n\u202a95.17B\u202c'
+        mock_raw13.text = 'Discontinued operations\n\u202a0.00\u202c\n—'
+        mock_raw14.text = 'Net income\nYoY growth\n\u202a30.00B\u202c\n−13.38%\n\u202a95.17B\u202c'
+        mock_raw15.text = 'Dilution adjustment\n\u202a0.00\u202c\n—'
+        mock_raw16.text = 'Preferred dividends\n\u202a0.00\u202c\n—'
+        mock_raw17.text = 'Diluted net income available to common stockholders\n\u202a30.00B\u202c\n\u202a95.17B\u202c'
+        mock_raw18.text = 'Basic earnings per share (Basic EPS)\nYoY growth\n\u202a1.89\u202c\n−10.66%\n\u202a5.92\u202c'
+        mock_raw19.text = 'Diluted earnings per share (Diluted EPS)\nYoY growth\n\u202a1.88\u202c\n−10.31%\n\u202a5.89\u202c'
+        mock_raw20.text = 'Average basic shares outstanding\n\u202a15.89B\u202c\n—'
+        mock_raw21.text = 'Diluted shares outstanding\n\u202a15.96B\u202c\n—'
+        mock_raw22.text = 'EBITDA\nYoY growth\n\u202a38.93B\u202c\n−11.89%\n\u202a125.29B\u202c'
+        mock_raw23.text = 'EBIT\nYoY growth\n\u202a36.02B\u202c\n−13.19%\n\u202a113.97B\u202c'
+        mock_raw24.text = 'Total operating expenses\n\u202a81.14B\u202c\n\u202a273.57B\u202c'
+
+        mock_driver.find_elements.side_effect = [
+            [mock_header], [mock_raw1], [mock_raw2], [mock_raw3], [mock_raw4],
+            [mock_raw5], [mock_raw6], [mock_raw7], [mock_raw8], [mock_raw9],
+            [mock_raw10], [mock_raw11], [mock_raw12], [mock_raw13], [mock_raw14],
+            [mock_raw15], [mock_raw16], [mock_raw17], [mock_raw18], [mock_raw19],
+            [mock_raw20],[mock_raw21],[mock_raw22],[mock_raw23],[mock_raw24]
+        ]
+
+        data = {'Ticker':('AAV'),'Total revenue':'117.15B','YoY growth Total revenue':'\u22125.48%',
+                'Cost of goods sold':'\u221266.82B',
+                'Gross profit':'50.33B','YoY growth Gross profit':'−7.21%',
+                'Operating expenses (excl. COGS)':'−14.32B',
+                'Operating income':'36.02B','YoY growth Operating income' : '−13.19%',
+                'Non-operating income, total':'−393.00M',
+                'Pretax income':'35.62B','YoY growth Pretax income':'−13.62%',
+                'Equity in earnings':'0.00','Taxes':'−5.63B',
+                'Non-controlling/minority interest':'0.00','After tax other income/expense':'0.00',
+                'Net income before discontinued operations':'30.00B','Discontinued operations':'0.00',
+                'Net income':'30.00B','YoY growth Net income':'−13.38%',
+                'Dilution adjustment':'0.00','Preferred dividends':'0.00','Diluted net income available to common stockholders':'30.00B',
+                'Basic EPS':'1.89','YoY growth Basic EPS':'−10.66%','Diluted EPS':'1.88','Diluted EPS YoY growth':'−10.31%',
+                'Average basic shares outstanding':'15.89B','Diluted shares outstanding':'15.96B',
+                'EBITDA':'38.93B','YoY growth EBITDA':'−11.89%','EBIT':'36.02B','YoY growth EBIT': '−13.19%',
+                'Total operating expenses':'81.14B','Year':"2022"}
+        
+        test = pd.DataFrame(data,index=[0])
+
+        # Set up the mock DataFrame
+        df = pd.DataFrame({'Index':['SET100']})
+        mock_read_sql.return_value = df
+        result = self.stock.download_quarter('AAV',False)
+        assert_frame_equal(result, test)
+
+    @patch('pandas.read_sql')
+    def test_download_quarter_nasdaq(self,mock_read_sql):
+        webdriver.Chrome = MagicMock()
+        mock_driver = MagicMock()
+        webdriver.Chrome.return_value = mock_driver
+        # Set up the mock DataFrame
+        df = pd.DataFrame({'Index':['NASDAQ']})
+        mock_read_sql.return_value = df
+        result = self.stock.download_quarter('AAPL',False)
+        mock_driver.get.assert_called_once_with('https://www.tradingview.com/symbols/NASDAQ-AAPL/financials-income-statement/')
+
+    @patch('pandas.read_sql')
+    def test_download_quarter_set(self,mock_read_sql):
+        webdriver.Chrome = MagicMock()
+        mock_driver = MagicMock()
+        webdriver.Chrome.return_value = mock_driver
+        # Set up the mock DataFrame
+        df = pd.DataFrame({'Index':['SET100']})
+        mock_read_sql.return_value = df
+        result = self.stock.download_quarter('AAV',False)
+        mock_driver.get.assert_called_once_with('https://www.tradingview.com/symbols/SET-AAV/financials-income-statement/')
+    
+    @patch('pandas.read_sql')
+    def test_download_quarter(self,mock_read_sql):
+        webdriver.Chrome = MagicMock()
+        mock_driver = MagicMock()
+        webdriver.Chrome.return_value = mock_driver
+        mock_header = MagicMock()
+        mock_raw1 = MagicMock()
+        mock_raw2 = MagicMock()
+        mock_raw3 = MagicMock()
+        mock_raw4 = MagicMock()
+        mock_raw5 = MagicMock()
+        mock_raw6 = MagicMock()
+        mock_raw7 = MagicMock()
+        mock_raw8 = MagicMock()
+        mock_raw9 = MagicMock()
+        mock_raw10 = MagicMock()
+        mock_raw11 = MagicMock()
+        mock_raw12 = MagicMock()
+        mock_raw13 = MagicMock()
+        mock_raw14 = MagicMock()
+        mock_raw15 = MagicMock()
+        mock_raw16 = MagicMock()
+        mock_raw17 = MagicMock()
+        mock_raw18 = MagicMock()
+        mock_raw19 = MagicMock()
+        mock_raw20 = MagicMock()
+        mock_raw21 = MagicMock()
+        mock_raw22 = MagicMock()
+        mock_raw23 = MagicMock()
+        mock_raw24 = MagicMock()
+
+        mock_header.text = "Currency: USD\nQ1 '23\nTTM"
+        mock_raw1.text = 'Total revenue\nYoY growth\n\u202a117.15B\u202c\n−5.48%\n\u202a387.54B\u202c'
+        mock_raw2.text = 'Cost of goods sold\n\u202a−66.82B\u202c\n\u202a−220.67B\u202c'
+        mock_raw3.text = 'Gross profit\nYoY growth\n\u202a50.33B\u202c\n−7.21%\n\u202a166.87B\u202c'
+        mock_raw4.text = 'Operating expenses (excl. COGS)\n\u202a−14.32B\u202c\n\u202a−52.91B\u202c'
+        mock_raw5.text = 'Operating income\nYoY growth\n\u202a36.02B\u202c\n−13.19%\n\u202a113.97B\u202c'
+        mock_raw6.text = 'Non-operating income, total\n\u202a−393.00M\u202c\n\u202a−480.00M\u202c'
+        mock_raw7.text = 'Pretax income\nYoY growth\n\u202a35.62B\u202c\n−13.62%\n\u202a113.48B\u202c'
+        mock_raw8.text = 'Equity in earnings\n\u202a0.00\u202c\n—'
+        mock_raw9.text = 'Taxes\n\u202a−5.63B\u202c\n\u202a−18.31B\u202c'
+        mock_raw10.text = 'Non-controlling/minority interest\n\u202a0.00\u202c\n—'
+        mock_raw11.text = 'After tax other income/expense\n\u202a0.00\u202c\n—'
+        mock_raw12.text = 'Net income before discontinued operations\n\u202a30.00B\u202c\n\u202a95.17B\u202c'
+        mock_raw13.text = 'Discontinued operations\n\u202a0.00\u202c\n—'
+        mock_raw14.text = 'Net income\nYoY growth\n\u202a30.00B\u202c\n−13.38%\n\u202a95.17B\u202c'
+        mock_raw15.text = 'Dilution adjustment\n\u202a0.00\u202c\n—'
+        mock_raw16.text = 'Preferred dividends\n\u202a0.00\u202c\n—'
+        mock_raw17.text = 'Diluted net income available to common stockholders\n\u202a30.00B\u202c\n\u202a95.17B\u202c'
+        mock_raw18.text = 'Basic earnings per share (Basic EPS)\nYoY growth\n\u202a1.89\u202c\n−10.66%\n\u202a5.92\u202c'
+        mock_raw19.text = 'Diluted earnings per share (Diluted EPS)\nYoY growth\n\u202a1.88\u202c\n−10.31%\n\u202a5.89\u202c'
+        mock_raw20.text = 'Average basic shares outstanding\n\u202a15.89B\u202c\n—'
+        mock_raw21.text = 'Diluted shares outstanding\n\u202a15.96B\u202c\n—'
+        mock_raw22.text = 'EBITDA\nYoY growth\n\u202a38.93B\u202c\n−11.89%\n\u202a125.29B\u202c'
+        mock_raw23.text = 'EBIT\nYoY growth\n\u202a36.02B\u202c\n−13.19%\n\u202a113.97B\u202c'
+        mock_raw24.text = 'Total operating expenses\n\u202a81.14B\u202c\n\u202a273.57B\u202c'
+
+        mock_driver.find_elements.side_effect = [
+            [mock_header], [mock_raw1], [mock_raw2], [mock_raw3], [mock_raw4],
+            [mock_raw5], [mock_raw6], [mock_raw7], [mock_raw8], [mock_raw9],
+            [mock_raw10], [mock_raw11], [mock_raw12], [mock_raw13], [mock_raw14],
+            [mock_raw15], [mock_raw16], [mock_raw17], [mock_raw18], [mock_raw19],
+            [mock_raw20],[mock_raw21],[mock_raw22],[mock_raw23],[mock_raw24]
+        ]
+
+        data = {'Ticker':('AAV'),'Total revenue':'117.15B','YoY growth Total revenue':'\u22125.48%',
+                'Cost of goods sold':'\u221266.82B',
+                'Gross profit':'50.33B','YoY growth Gross profit':'−7.21%',
+                'Operating expenses (excl. COGS)':'−14.32B',
+                'Operating income':'36.02B','YoY growth Operating income' : '−13.19%',
+                'Non-operating income, total':'−393.00M',
+                'Pretax income':'35.62B','YoY growth Pretax income':'−13.62%',
+                'Equity in earnings':'0.00','Taxes':'−5.63B',
+                'Non-controlling/minority interest':'0.00','After tax other income/expense':'0.00',
+                'Net income before discontinued operations':'30.00B','Discontinued operations':'0.00',
+                'Net income':'30.00B','YoY growth Net income':'−13.38%',
+                'Dilution adjustment':'0.00','Preferred dividends':'0.00','Diluted net income available to common stockholders':'30.00B',
+                'Basic EPS':'1.89','YoY growth Basic EPS':'−10.66%','Diluted EPS':'1.88','Diluted EPS YoY growth':'−10.31%',
+                'Average basic shares outstanding':'15.89B','Diluted shares outstanding':'15.96B',
+                'EBITDA':'38.93B','YoY growth EBITDA':'−11.89%','EBIT':'36.02B','YoY growth EBIT': '−13.19%',
+                'Total operating expenses':'81.14B','Quarterly':"Q1 '23"}
+        
+        test = pd.DataFrame(data,index=[0])
+
+        # Set up the mock DataFrame
+        df = pd.DataFrame({'Index':['SET100']})
+        mock_read_sql.return_value = df
+        result = self.stock.download_quarter('AAV',False)
+        assert_frame_equal(result, test)
+
     @patch('selenium.webdriver.Chrome')
     @patch('pandas_datareader.nasdaq_trader.get_nasdaq_symbols')
     @patch('sqlite3.connect')
@@ -1395,7 +1635,6 @@ class ML_stock:
         place.to_sql('stock_city',con=conn,if_exists='append',index=False)
         return place
 
-    ##No test
     def download_info(self,Ticker):
         conn = sqlite3.connect("stock.sqlite")
         exchange = ccxt.binance()
@@ -1441,7 +1680,6 @@ class ML_stock:
         df_d.to_sql('stock_table_d',con=conn,if_exists='append',index=True)
         df_mo.to_sql('stock_table_mo',con=conn,if_exists='append',index=True)
 
-    ##No test
     def download_year(self,ticker,save):
         test = []
         test2 = []
@@ -1498,33 +1736,36 @@ class ML_stock:
 
         YoY = [''] * len(Allelement)
         Oth = [''] * len(Allelement)
-        for element in header:
-            test.append(element.text)
-        year = test[0].split('\n')[1:-1]
-        for e in Allelement:
-            revenue = []
-            YoYR = []
-            test2 = []
-            for h in e:
-                test2.append(h.text)
-            test2 = test2[0].split('\n')
-            if (e == raw1) or (e == raw3) or (e == raw5) or (e == raw7) or (e == raw14) or (e == raw18) or (e == raw19) or (e == raw22) or (e == raw23):
-                for k in range(len(test2)-1):
-                    if k%2 == 0 and k > 0:
-                        YoYR.append(test2[k])
-                    else:
-                        if k > 0 :
-                            a = test2[k]
-                            revenue.append(a[1:-1])
-            else:
-                for k in range(len(test2)-2):
-                    a = test2[k+1]
-                    YoYR.append(None)
-                    revenue.append(a[1:-1])
+        try:
+            for element in header:
+                test.append(element.text)
+            year = test[0].split('\n')[1:-1]
+            for e in Allelement:
+                revenue = []
+                YoYR = []
+                test2 = []
+                for h in e:
+                    test2.append(h.text)
+                test2 = test2[0].split('\n')
+                if (e == raw1) or (e == raw3) or (e == raw5) or (e == raw7) or (e == raw14) or (e == raw18) or (e == raw19) or (e == raw22) or (e == raw23):
+                    for k in range(len(test2)-1):
+                        if k%2 == 0 and k > 0:
+                            YoYR.append(test2[k])
+                        else:
+                            if k > 0 :
+                                a = test2[k]
+                                revenue.append(a[1:-1])
+                else:
+                    for k in range(len(test2)-2):
+                        a = test2[k+1]
+                        YoYR.append(None)
+                        revenue.append(a[1:-1])
 
-            YoY[count] = (YoYR)
-            Oth[count] = (revenue)
-            count += 1
+                YoY[count] = (YoYR)
+                Oth[count] = (revenue)
+                count += 1
+        except:
+            return False
         a = range(len(Oth))
         s = '2018'
         for i in range(len(year)):
@@ -1560,7 +1801,6 @@ class ML_stock:
             thf2.to_sql('stock_financial',con=conn,if_exists='append',index=False)
         return thf2
 
-    ##No test
     def download_quarter(self,ticker,save):
         test = []
         test2 = []
@@ -1615,33 +1855,35 @@ class ML_stock:
 
         YoY = [''] * len(Allelement)
         Oth = [''] * len(Allelement)
-        for element in header:
-            test.append(element.text)
-        year = test[0].split('\n')[1:-1]
-        for e in Allelement:
-            test2 = []
-            for h in e:
-                test2.append(h.text)
-            test2 = test2[0].split('\n')
-            if (e == raw1) or (e == raw3) or (e == raw5) or (e == raw7) or (e == raw14) or (e == raw18) or (e == raw19) or (e == raw22) or (e == raw23):
-                for k in range(len(test2)-3):
-                    if k%2 == 0:
-                        a = test2[k+2]
+        try:
+            for element in header:
+                test.append(element.text)
+            year = test[0].split('\n')[1:-1]
+            for e in Allelement:
+                test2 = []
+                for h in e:
+                    test2.append(h.text)
+                test2 = test2[0].split('\n')
+                if (e == raw1) or (e == raw3) or (e == raw5) or (e == raw7) or (e == raw14) or (e == raw18) or (e == raw19) or (e == raw22) or (e == raw23):
+                    for k in range(len(test2)-3):
+                        if k%2 == 0:
+                            a = test2[k+2]
+                            revenue.append(a[1:-1])
+                        else:
+                            YoYR.append(test2[k+2])
+                else:
+                    for k in range(len(test2)-2):
+                        a = test2[k+1]
                         revenue.append(a[1:-1])
-                    else:
-                        YoYR.append(test2[k+2])
-            else:
-                for k in range(len(test2)-2):
-                    a = test2[k+1]
-                    revenue.append(a[1:-1])
-                    YoYR.append(None)
+                        YoYR.append(None)
 
-            YoY[count] = (YoYR)
-            Oth[count] = (revenue)
-            count += 1
-            YoYR = []
-            revenue = []
-
+                YoY[count] = (YoYR)
+                Oth[count] = (revenue)
+                count += 1
+                YoYR = []
+                revenue = []
+        except:
+            return False
         a = range(len(Oth))
         for i in range(len(year)):
             try:
